9a45cbb1e4d63cbe7d8b237a3bb2554defb20c59
dorokhina <dorokhina@medesk.md>
2024-07-15 13:48:46 +0300

    short readme is added

--- src/main/java/org/example/LargeFileReader.java
+++ src/main/java/org/example/LargeFileReader.java
@@ -10,39 +10,40 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class LargeFileReader {
-    protected final String TEST_FILE_PATH = "QAtest.txt";
+    protected static final String TEST_FILE_PATH = "QAtest.txt";
     protected final String SUBSTRING = "a";
 
     public static void main(String[] args) {
+        File file = new File(TEST_FILE_PATH);
+
         new LargeFileReader().getTotalCountOfRowsUsingStream();
-        new LargeFileReader().getTotalCountOfRowsUsingBufferedReader();
+        new LargeFileReader().getTotalCountOfRowsUsingBufferedReader(file);
         new LargeFileReader().getCountOfMatchesUsingStream();
-        new LargeFileReader().getTotalCountOfMatchesUsingBufferedreader();
+        new LargeFileReader().getTotalCountOfMatchesUsingBufferedreader(file);
     }
 
     public void getTotalCountOfRowsUsingStream(){
-        long total = 0;
+        long totalTime = 0;
         long begin = System.currentTimeMillis();
             try (Stream<String> lines = java.nio.file.Files.lines(Paths.get(TEST_FILE_PATH))) {
                 System.out.printf("Count of rows: %s rows", lines.count());
                 System.out.println();
 
-                total += System.currentTimeMillis() - begin;
+                totalTime += System.currentTimeMillis() - begin;
 
-                System.out.printf("Time for counting of rows using Stream: %s ms", total);
+                System.out.printf("Time for counting of rows using Stream: %s ms", totalTime);
                 System.out.println();
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
     }
 
-    public void getTotalCountOfMatchesUsingBufferedreader(){
-        File file = new File(TEST_FILE_PATH);
+    public void getTotalCountOfMatchesUsingBufferedreader(File file){
         try (
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);
         ) {
-            long total = 0;
+            long totalTime = 0;
             long begin = System.currentTimeMillis();
              String line = br.readLine();
              long totalCountOfMatches = 0;
@@ -53,30 +54,32 @@ public class LargeFileReader {
                  line = br.readLine();
              }
 
-            total += System.currentTimeMillis() - begin;
-            System.out.println("Total count of matches: %s matches" + totalCountOfMatches);
-            System.out.printf("Time for counting of matches using Buffered Reader: %s ms", total);
+            totalTime += System.currentTimeMillis() - begin;
+
+            System.out.printf("Total count of matches: %s matches", totalCountOfMatches);
+            System.out.println();
+
+            System.out.printf("Time for counting of matches using Buffered Reader: %s ms", totalTime);
             System.out.println();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void getTotalCountOfRowsUsingBufferedReader(){
-        File file = new File(TEST_FILE_PATH);
+    public void getTotalCountOfRowsUsingBufferedReader(File file){
         try (
                 FileReader fr = new FileReader(file);
                 BufferedReader br = new BufferedReader(fr);
         ) {
-            long total = 0;
+            long totalTime = 0;
             long begin = System.currentTimeMillis();
 
             System.out.printf("Total count of rows: %s rows", br.lines().count());
             System.out.println();
 
-            total += System.currentTimeMillis() - begin;
+            totalTime += System.currentTimeMillis() - begin;
 
-            System.out.printf("Time for counting of matches using Buffered Reader: %s ms", total);
+            System.out.printf("Time for counting of matches using Buffered Reader: %s ms", totalTime);
             System.out.println();
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -84,7 +87,7 @@ public class LargeFileReader {
     }
 
     public void getCountOfMatchesUsingStream(){
-        long total = 0;
+        long totalTime = 0;
         long begin = System.currentTimeMillis();
         try (Stream<String> lines = java.nio.file.Files.lines(Paths.get(TEST_FILE_PATH))) {
             long count = lines.mapToLong(line ->
@@ -92,9 +95,9 @@ public class LargeFileReader {
             System.out.printf("Count of substring is: %s pcs.", count);
             System.out.println();
 
-            total += System.currentTimeMillis() - begin;
+            totalTime += System.currentTimeMillis() - begin;
 
-            System.out.printf("Time for counting of matches using Stream: %s ms", total);
+            System.out.printf("Time for counting of matches using Stream: %s ms", totalTime);
             System.out.println();
         } catch (IOException e) {
             throw new RuntimeException(e);
